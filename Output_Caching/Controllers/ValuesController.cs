64ec150d2f0bbf3278dae6b99eebb94b20164b3f
Gençay Yıldız <gyildizmail@gmail.com>
2022-11-17 22:29:58 +0300

    Output Caching

---
+++ Output_Caching/Controllers/ValuesController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OutputCaching;
+
+namespace Output_Caching.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [OutputCache(PolicyName = "Custom")]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        [OutputCache]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ Output_Caching/Program.cs
@@ -0,0 +1,70 @@
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.OutputCaching;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddOutputCache(options =>
+{
+    options.AddBasePolicy(builder =>
+    {
+        builder.Expire(TimeSpan.FromSeconds(5));
+    });
+
+    options.AddPolicy("Custom", policy =>
+    {
+        policy.Expire(TimeSpan.FromSeconds(10));
+    });
+});
+
+var app = builder.Build();
+
+app.UseOutputCache();
+
+app.MapGet("/", () =>
+{
+    return Results.Ok(DateTime.UtcNow);
+}).CacheOutput();
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
+
+
+#region Output Caching
+
+#region Output Cache'i Pipeline'a Ekleme
+
+#endregion
+#region Minimal API'larýn Cache'lenmesi
+#region CacheOutput Metodu
+
+#endregion
+#region OutputCache Attribute'u
+
+#endregion
+#endregion
+#region Controller'da Cache'leme
+
+#endregion
+#region Cache Politikasý Oluþturma
+
+#endregion
+
+#endregion
